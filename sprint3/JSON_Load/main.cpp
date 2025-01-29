82da968e2f4f1a6e55762731e5203905e9d5f12a
part-of-the-crew <nikolai.dunichev@gmail.com>
2025-01-29 21:22:43 +0200

    Add json

---
+++ sprint3/JSON_Load/main.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+#include "json.h"
+
+using namespace std;
+
+struct Spending {
+    string category;
+    int amount;
+};
+
+int CalculateTotalSpendings(const vector<Spending>& spendings) {
+    return accumulate(
+        spendings.begin(), spendings.end(), 0, [](int current, const Spending& spending){
+            return current + spending.amount;
+        });
+}
+
+string FindMostExpensiveCategory(const vector<Spending>& spendings) {
+    assert(!spendings.empty());
+    auto compare_by_amount = [](const Spending& lhs, const Spending& rhs) {
+        return lhs.amount < rhs.amount;
+    };
+    return max_element(begin(spendings), end(spendings), compare_by_amount)->category;
+}
+
+vector<Spending> LoadFromJson(istream& input) {
+    Document doc = Load(input);
+    vector<Spending> result;
+    for (const Node& node : doc.GetRoot().AsArray()) {
+        result.push_back({node.AsMap().at("category"s).AsString(),  //
+                          node.AsMap().at("amount"s).AsInt()});
+    }
+    return result;
+}
+
+int main() {
+    const vector<Spending> spendings = LoadFromJson(cin);
+    cout << "Total "sv << CalculateTotalSpendings(spendings) << '\n';
+    cout << "Most expensive is "sv << FindMostExpensiveCategory(spendings) << '\n';
+}
