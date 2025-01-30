d646388e607b32fec865ff507e1eeec5ac65f16a
part-of-the-crew <nikolai.dunichev@gmail.com>
2025-01-30 10:19:45 +0200

    Add XML

---
+++ sprint3/XML_Load/main.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+#include "xml.h"
+
+using namespace std;
+
+struct Spending {
+    string category;
+    int amount;
+};
+
+int CalculateTotalSpendings(const vector<Spending>& spendings) {
+    return accumulate(spendings.begin(), spendings.end(), 0,
+                      [](int current, const Spending& spending) {
+                          return current + spending.amount;
+                      });
+}
+
+string FindMostExpensiveCategory(const vector<Spending>& spendings) {
+    assert(!spendings.empty());
+    auto compare_by_amount = [](const Spending& lhs, const Spending& rhs) {
+        return lhs.amount < rhs.amount;
+    };
+    return max_element(begin(spendings), end(spendings), compare_by_amount)->category;
+}
+
+vector<Spending> LoadFromXml(istream& input) {
+    const Document doc = Load(input);
+    const Node root = doc.GetRoot();
+    vector<Spending> result;
+
+    for (auto const& ch: root.Children()){
+        result.push_back({ch.AttributeValue<string>("category"s), ch.AttributeValue<int>("amount"s)});
+    }
+
+    return result;
+}
+
+int main() {
+    const vector<Spending> spendings = LoadFromXml(cin);
+    cout << "Total "sv << CalculateTotalSpendings(spendings) << '\n';
+    cout << "Most expensive is "sv << FindMostExpensiveCategory(spendings) << '\n';
+}
