201fe86cf5f3bda8ce53a179c0e31c92de194e0c
part-of-the-crew <nikolai.dunichev@gmail.com>
2025-02-21 16:27:29 +0200

    Tiles started

--- sprint3/CanvasShapes/shapes.h
+++ sprint3/CanvasShapes/shapes.h
@@ -3,13 +3,11 @@
 #include <iostream>
 #include <memory>
 
-// Поддерживаемые виды фигур: прямоугольник и эллипс
 enum class ShapeType { RECTANGLE, ELLIPSE};
 
 class Shape {
 public:
-    // Фигура после создания имеет нулевые координаты и размер,
-    // а также не имеет текстуры
+
     explicit Shape(ShapeType type)
         : type_(type)
         , pos_({0, 0})
@@ -20,39 +18,17 @@ public:
 
     void SetPosition(Point pos) {
         pos_ = pos;
-        // Заглушка. Реализуйте метод самостоятельно
     }
 
     void SetSize(Size size) {
         size_ = size;
-        // Заглушка. Реализуйте метод самостоятельно
     }
 
     void SetTexture(std::shared_ptr<Texture> texture) {
         texture_ = texture;
-        // Заглушка. Реализуйте метод самостоятельно
     }
 
-    // Рисует фигуру на указанном изображении
-	// В зависимости от типа фигуры должен рисоваться либо эллипс, либо прямоугольник
-    // Пиксели фигуры, выходящие за пределы текстуры, а также в случае, когда текстура не задана,
-    // должны отображаться с помощью символа точка '.'
-    // Части фигуры, выходящие за границы объекта image, должны отбрасываться.
     void Draw(Image& image) const {
-        // Заглушка. Реализуйте метод самостоятельно
-        /*
-        2.  как раз в методе Draw нужно и размещать фигуру на полотно - 
-        и выбирать какие цвета из текстуры брать. 
-        Нужно циклами пройтись по всем возможным точкам :
-        for (int y = 0; y < size_.height; ++y) {
-            for (int x = 0; x < size_.width; ++x)
-        и рассматривать расположение точек {x, y} и {x + pos_.x, y + pos_.y} - расположены ли они внутри фигуры? 
-        внутри полотна? внутри текстуры? - и в зависимости от этого уже определять какой именно цвет 
-        присваивать image в точке {x + pos_.x, y + pos_.y}
-        1 this image to draw
-        2 image in texture
-
-        */
         Size tsize;
         if (!texture_) {
             tsize = {0,0};
@@ -61,17 +37,8 @@ public:
         }
         
         auto isize = GetImageSize(image);
-        //if (isize.height == 0 || isize.width == 0 || tsize.height == 0 || tsize.width == 0)
-        //    return;
-
-        //auto pixel_color = texture_->GetPixelColor({0, 0});
-        // Заполняем полотно цветом фигуры
-        //std::cout << pos_.x << " " << pos_.y << texture_->GetPixelColor({pos_.x, pos_.y}) << std::endl;
-        //for (int y = 0; y < size_.height; ++y) {
-            //for (int x = 0; x < size_.width; ++x) {
         for (int y = pos_.y; y < std::min(size_.height + pos_.y, isize.height); ++y) {
             for (int x = pos_.x; x < std::min(size_.width + pos_.x, isize.width); ++x) {
-                //image.at(y).at(x) =  ' ';
                 if (type_ == ShapeType::RECTANGLE) {
                     if (tsize.height <= y - pos_.y || tsize.width <= x - pos_.x) {
                         image.at(y).at(x) = '.';
@@ -96,5 +63,5 @@ private:
     ShapeType type_;
     Point pos_ = {};
     Size size_ = {};
-    std::shared_ptr<Texture> texture_ = nullptr;  // Указатель на текстуру, может быть nullptr
+    std::shared_ptr<Texture> texture_ = nullptr;
 };
--- sprint3/CanvasShapes/texture.h
+++ sprint3/CanvasShapes/texture.h
@@ -7,14 +7,10 @@ public:
         : image_(std::move(image)) {
     }
 
-    // Возвращает размер изображения
     Size GetSize() const {
         return GetImageSize(image_);
-        //return {static_cast<int>(image_.front().size()), static_cast<int>(image_.size())};
     }
 
-    // Возвращает цвет пикселя.
-    // Если координаты выходят за границы изображения, возвращается пробел
     char GetPixelColor(Point p) const {
         
         if (p.x < 0 || p.x >= static_cast<int>(image_.front().size()) ||
