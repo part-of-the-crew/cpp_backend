5cfc25216953327aa5771f54fb25a09472bbe025
part-of-the-crew <amente4one@gmail.com>
2025-05-03 20:44:30 +0300

    emploees

---
+++ sprint4/emploees/main.cpp
@@ -0,0 +1,27 @@
+#include <cassert>
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <exception>
+
+using namespace std;
+
+#include "people.h"
+
+
+int main() {
+    using namespace std;
+
+    try {
+        Programmer programmer{"Bjarne Stroustrup"s, 69, Gender::MALE};
+        programmer.AddProgrammingLanguage(ProgrammingLanguage::CPP);
+        assert(programmer.CanProgram(ProgrammingLanguage::CPP));
+
+        Worker worker{"Mario"s, 42, Gender::MALE};
+        worker.AddSpeciality(WorkerSpeciality::PLUMBER);
+        assert(worker.HasSpeciality(WorkerSpeciality::PLUMBER));
+    } catch (...) {
+        cout << "Exception was thrown"s << endl;
+    }
+    cout << "OK"s << endl;
+}
