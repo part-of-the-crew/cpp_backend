81271cb7ec5064a0284715199a2637f1b1b77176
part-of-the-crew <nikolai.dunichev@gmail.com>
2025-08-05 11:11:42 +0300

    done domain_filter with tests

--- sprint4/domain_filter/main.cpp
+++ sprint4/domain_filter/main.cpp
@@ -8,6 +8,7 @@
 #include <ranges>
 #include <concepts>
 #include <set>
+#include <cassert>
 
 using namespace std;
 
@@ -31,24 +32,26 @@ vector<string> SplitIntoWords(const string& text) {
     return words;
 }
 
+
     // разработайте класс домена
 class Domain {
     vector<string> domain_;
 
     template <typename It1, typename It2, typename Eq = std::equal_to<>>
-        bool is_part_of_container(const It1& begin1, const It1& end1, 
-                        const It2& begin2, const It2& end2, Eq eq = {}) const {
-        auto it1 = begin1;
-        auto it2 = begin2;
-        for (; it1 != end1 && it2 != end2; ++it1, ++it2) {
-            if (!eq(*it1, *it2)) return false;
+        bool is_short_part_of_long( const It1& short_begin, const It1& short_end, 
+                       const It2& long_begin, const It2& long_end, Eq eq = {}) const {
+        auto it_short = short_begin;
+        auto it_long = long_begin;
+        for (; it_short != short_end && it_long != long_end; ++it_short, ++it_long) {
+            if (!eq(*it_short, *it_long)) return false;
         }
-        if (it1 == end1 && it2 == end2)
+        if (it_short == short_end && it_long == long_end)
             return true;
-        if (it1 == end1)
+        if (it_short == short_end)
             return false;
         return true;
     }
+
 public:
     // конструктор должен позволять конструирование из string, с сигнатурой определитесь сами
     Domain (const string& str)
@@ -64,10 +67,10 @@ public:
     }
     // разработайте метод IsSubdomain, принимающий другой домен и возвращающий true, если this его поддомен
     bool IsSubdomain(const Domain& sub) const {
-        return is_part_of_container(sub.domain_.begin(), sub.domain_.end(), domain_.begin(), domain_.end());
+        return is_short_part_of_long(sub.domain_.begin(), sub.domain_.end(), domain_.begin(), domain_.end());
     }
     bool IsDomain(const Domain& dom) const {
-        return is_part_of_container(domain_.begin(), domain_.end(), dom.domain_.begin(), dom.domain_.end());
+        return is_short_part_of_long(domain_.begin(), domain_.end(), dom.domain_.begin(), dom.domain_.end());
     }
     friend std::ostream& operator<<(std::ostream& os, const Domain& d) {
         // domain_ is reversed; print in normal order
@@ -98,7 +101,6 @@ public:
         vector<Domain> banned_domains(begin, end);
         sort(banned_domains.begin(), banned_domains.end());
         for (auto const& e: banned_domains){
-            //cout << e << endl;
             if (banned_domains_.empty()){
                 banned_domains_.emplace_back(e);
             } else {
@@ -155,7 +157,69 @@ Number ReadNumberOnLine(istream& input) {
     return num;
 }
 
+
+inline void Tests() {
+
+    {
+        // Simulated program-created variable
+        std::string simulatedInput = "  4   \n";
+        // Wrap the string in an input stream
+        std::istringstream inputStream(simulatedInput);
+        auto num = ReadNumberOnLine<size_t>(inputStream);
+        assert(num == 4u);
+    }
+
+    {
+        // Simulated program-created variable
+        std::string simulatedInput = "com1\nexample1.com1\nexample.com\ncom\n";
+        // Wrap the string in an input stream
+        std::istringstream inputStream(simulatedInput);
+        auto v1 = ReadDomains<size_t>(inputStream, 4u);
+        const std::vector<Domain> v2 {"com1"s,"example1.com1"s, "example.com"s, "com"s};
+        assert(v1 == v2);
+    }
+
+    {
+        const std::vector<Domain> v {"com"s};
+        DomainChecker checker1{v.begin(), v.end()};
+        assert(checker1.GetSize() == 1u);
+    }
+    
+    
+    {
+        auto vec = SplitIntoWords("example.com");
+        assert(vec.size() == 2u);
+        assert(vec[0] == "example");
+        assert(vec[1] == "com");
+    }
+
+    {
+        auto domain1 = Domain{"example.com"};
+        auto domain1_ = Domain{"example.com"};
+        auto domain2 = Domain{"com"};
+
+        assert(domain1 == domain1_);
+        assert(!(domain1 == domain2));
+        assert(domain2.IsSubdomain(domain1));
+        assert(!domain1.IsSubdomain(domain2));
+    }
+
+    {
+        const std::vector<Domain> v {"com1"s,"example1.com1"s, "example.com"s, "com"s};
+        DomainChecker checker{v.begin(), v.end()};
+        assert(checker.GetSize() == 2u);
+        assert(checker.IsForbidden("com"s));
+        assert(checker.IsForbidden("any.com"s));
+        assert(!checker.IsForbidden("example.com2"s));
+    }
+
+
+}
+
 int main() {
+
+    Tests();
+
     const std::vector<Domain> forbidden_domains = ReadDomains(cin, ReadNumberOnLine<size_t>(cin));
     DomainChecker checker(forbidden_domains.begin(), forbidden_domains.end());
 
@@ -163,11 +227,4 @@ int main() {
     for (const Domain& domain : test_domains) {
         cout << (checker.IsForbidden(domain) ? "Bad"sv : "Good"sv) << endl;
     }
-/*
-    Tests
-    const std::vector<Domain> v {"com1"s,"example1.com1"s,  "example.com"s, "com"s};
-    DomainChecker checker1{v.begin(), v.end()};
-    //hecker1.Print();
-    //cout << checker1.GetSize() << endl;
-*/
 }
