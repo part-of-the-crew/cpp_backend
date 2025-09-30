513ad98872e104efb893d601d43258c756eaa6fb
part-of-the-crew <nikolai.dunichev@gmail.com>
2025-09-30 19:56:22 +0300

    using_libJpeg cmake is done

--- sprint5/using_libJpeg/test.sh
+++ sprint5/using_libJpeg/test.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-g++ hmirr.cpp ppm_image.cpp img_lib.cpp
-./a.out ./test_cases/patrik.ppm ./test_cases/_.ppm
+mkdir build
+cd build
+
+cmake ..
+cmake --build .
+
+cd ..
+./build/hmirror ./test_cases/patrik.ppm ./test_cases/_.ppm
 colordiff -yq ./test_cases/_.ppm ./test_cases/patrik_h.ppm
-    rm ./a.out ./test_cases/_.ppm
-g++ vmirr.cpp ppm_image.cpp img_lib.cpp
-./a.out ./test_cases/patrik.ppm ./test_cases/_.ppm
+    rm ./test_cases/_.ppm
+
+./build/vmirror ./test_cases/patrik.ppm ./test_cases/_.ppm
 colordiff -yq ./test_cases/_.ppm ./test_cases/patrik_v.ppm
-    rm ./a.out ./test_cases/_.ppm
-g++ sobel.cpp ppm_image.cpp img_lib.cpp
-./a.out ./test_cases/patrik.ppm ./test_cases/_.ppm
+    rm ./test_cases/_.ppm
+
+./build/sobel ./test_cases/patrik.ppm ./test_cases/_.ppm
 colordiff -yq ./test_cases/_.ppm ./test_cases/patrik_s.ppm
-    rm ./a.out ./test_cases/_.ppm
+    rm ./test_cases/_.ppm
+
+rm -rf ./build
+
