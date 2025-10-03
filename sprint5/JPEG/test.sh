67c8235cc1afbb2581755885ddd72ad874d4c460
part-of-the-crew <nikolai.dunichev@gmail.com>
2025-10-03 22:05:56 +0300

    add JPEG

---
+++ sprint5/JPEG/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+mkdir build
+cd build
+
+
+
+cmake ../ImgConverter -DCMAKE_BUILD_TYPE=Debug -DLIBJPEG_DIR=/usr/lib/x86_64-linux-gnu/
+if [ $? -eq 1 ]; then
+    printf ${RED}___FAIL!!!___"cmake ../ImgConverter"${NC} && echo ""
+    cd ..
+    rm -rf ./build
+    exit 1
+fi
+
+cmake --build . --verbose
+
+cd ..
+./build/imgconv ./test_cases/reading_dog_ppm.ppm ./build/_.jpeg
+colordiff -yq ./build/_.jpeg ./test_cases/reading_dog_from_ppm.jpg
+
+if [ $? -eq 0 ]; then
+    printf ${GREEN}___PASS!___${NC}
+else
+    printf ${RED}___FAIL!!!___${NC}
+fi
+echo ""
+
+rm -rf ./build
+
