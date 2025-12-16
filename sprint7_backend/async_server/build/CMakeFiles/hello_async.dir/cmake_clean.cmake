0dcf8d2b79a9fa2e36eb1c9d7fd9c12cc6deda2a
part-of-the-crew <nikolai.dunichev@gmail.com>
2025-12-16 13:27:19 +0200

    async_server added

---
+++ sprint7_backend/async_server/build/CMakeFiles/hello_async.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_async.dir/src/http_server.cpp.o"
+  "CMakeFiles/hello_async.dir/src/http_server.cpp.o.d"
+  "CMakeFiles/hello_async.dir/src/main.cpp.o"
+  "CMakeFiles/hello_async.dir/src/main.cpp.o.d"
+  "bin/hello_async"
+  "bin/hello_async.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_async.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
