921cc8bf34531624c6d34a29981b668a6f2df081
part-of-the-crew <nikolai.dunichev@gmail.com>
2026-01-07 13:26:14 +0200

    add server_auth

--- sprint8_backend/server_auth/src/main.cpp
+++ sprint8_backend/server_auth/src/main.cpp
@@ -31,44 +31,6 @@ void RunWorkers(unsigned n, const Fn& fn) {
 
 }  // namespace
 
-template <class Handler>
-class LoggingRequestHandler {
-public:
-    explicit LoggingRequestHandler(Handler& handler) : handler_(handler) {}
-
-    template <class Request, class Send>
-    void operator()(tcp::endpoint& endpoint, Request&& req, Send&& send) {
-        LogRequest(req);
-
-        // Wrap send to intercept the response
-        auto logged_send = [this, &send](auto&& response) {
-            LogResponse(response);
-            send(std::forward<decltype(response)>(response));
-        };
-
-        handler_(std::forward<Request>(req), logged_send);
-    }
-
-private:
-    Handler& handler_;
-    std::chrono::system_clock::time_point start_ts_{};
-    template <class Request>
-    void LogRequest(const Request& req) {
-        start_ts_ = std::chrono::system_clock::now();
-    }
-
-    template <class Response>
-    void LogResponse(const Response& resp) {
-        auto ms = duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start_ts_);
-        std::string content_type = "unknown";
-        if (resp.find(boost::beast::http::field::content_type) != resp.end()) {
-            content_type = std::string(resp[boost::beast::http::field::content_type]);
-        }
-
-        logger::LogServerResponse(ms.count(), resp.result_int(), content_type);
-    }
-};
-
 int main(int argc, const char* argv[]) {
     if (argc == 1) {
         std::cerr << "Usage: game_server <game-config-json>"sv << std::endl;
@@ -87,7 +49,16 @@ int main(int argc, const char* argv[]) {
         // 2. Инициализируем io_context
         const unsigned num_threads = std::thread::hardware_concurrency();
         net::io_context ioc(num_threads);
+        auto api_strand = net::make_strand(ioc);
 
+        const auto address = net::ip::make_address("0.0.0.0");
+        constexpr net::ip::port_type port = 8080;
+
+        http_handler::RequestHandler handler{path_to_static, api_strand, game};
+
+        logger::LoggingRequestHandler logging_handler{handler};
+
+        http_server::ServeHttp(ioc, {address, port}, logging_handler);
         // 3. Добавляем асинхронный обработчик сигналов SIGINT и SIGTERM
         // Подписываемся на сигналы и при их получении завершаем работу сервера
         net::signal_set signals(ioc, SIGINT, SIGTERM);
@@ -111,24 +82,11 @@ int main(int argc, const char* argv[]) {
                 ioc.stop();
             }
         });
-        // 4. Создаём обработчик HTTP-запросов и связываем его с моделью игры
-        http_handler::RequestHandler handler{game, path_to_static};
-        LoggingRequestHandler logging_handler{handler};
-        //  5. Запустить обработчик HTTP-запросов, делегируя их обработчику запросов
-        const auto address = net::ip::make_address("0.0.0.0");
-        constexpr net::ip::port_type port = 8080;
 
-        http_server::ServeHttp(ioc, {address, port}, [&logging_handler](auto&& req, auto&& send) {
-            logging_handler(std::forward<decltype(req)>(req), std::forward<decltype(send)>(send));
-        });
-
-        // Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
-        // std::cout << "Server has started..."sv << std::endl;
         logger::LogServerLaunch({address, port});
         // 6. Запускаем обработку асинхронных операций
-        RunWorkers(std::max(1u, num_threads), [&ioc] { ioc.run(); });
+        RunWorkers(num_threads, [&ioc] { ioc.run(); });
     } catch (const std::exception& ex) {
-        // std::cerr << ex.what() << std::endl;std::exception::what()
         logger::LogServerStop(EXIT_FAILURE, ex.what());
         boost::log::core::get()->flush();  // Force write to console
         return EXIT_FAILURE;
