f958705cfc1a744e3f2e5af40ece917495480bc3
part-of-the-crew <nikolai.dunichev@gmail.com>
2026-01-09 16:09:01 +0200

    game state is done

---
+++ sprint8_backend/game_state/test.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+#set -e  # stop on first error
+
+# --- Configuration ---
+BUILD_DIR="build"
+EXECUTABLE="$BUILD_DIR/bin/game_server"  # change this to your target name
+ARGS="data/config.json static"
+# --- Helper functions ---
+configure() {
+    clang-format --style=file --assume-filename=../.clang-format -i ./src/*.cpp  ./src/*.h
+    mkdir $BUILD_DIR
+    echo "⚙️  Configuring Conan..."$BUILD_DIR"/"
+    cd $BUILD_DIR
+    conan install ..  --build=missing -s compiler.libcxx=libstdc++11 -s build_type=Debug
+    cd ..
+    echo "⚙️  Configuring CMake..."
+    cmake -S . -DCMAKE_BUILD_TYPE=Debug -B "$BUILD_DIR" 2>&1
+}
+
+build() {
+    echo "🔨 Building project..."
+    cmake --"$BUILD_DIR" "$BUILD_DIR" -- -j 2>&1
+    #setsid cmake --build "$BUILD_DIR" -j >/dev/null 2>&1 &
+}
+
+clean() {
+    echo "🧹 Cleaning build directory..."
+    rm -rf "$BUILD_DIR"
+}
+
+run() {
+    if [[ ! -x "$EXECUTABLE" ]]; then
+        echo "❌ Executable not found! Run ./build.sh build first."
+        exit 1
+    fi
+    echo "🚀 Running program..."
+    "$EXECUTABLE" $ARGS
+}
+runv() {
+    if [[ ! -x "$EXECUTABLE" ]]; then
+        echo "❌ Executable not found! Run ./build.sh build first."
+        exit 1
+    fi
+    echo "🚀 Running program..."
+    valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out$i.txt \
+        "$EXECUTABLE" 2>&1
+}
+
+# --- Main logic ---
+case "$1" in
+    configure)
+        configure
+        ;;
+    build)
+        build
+        ;;
+    clean)
+        clean
+        ;;
+    run)
+        shift
+        run "$@"
+        ;;
+    runv)
+        shift
+        runv "$@"
+        ;;
+    vrun)
+        shift
+        runv "$@"
+        ;;
+    *)
+        #clean
+        configure
+        #echo "cmake --build ./$BUILD_DIR"
+        build || exit 1
+        run
+        ;;
+esac
