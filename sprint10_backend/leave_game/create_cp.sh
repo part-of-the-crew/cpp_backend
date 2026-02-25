3b33378ce058a4df6c064bd62a023745e7f248f3
part-of-the-crew <nikolai.dunichev@gmail.com>
2026-02-25 15:41:09 +0200

    add leave_game

---
+++ sprint10_backend/leave_game/create_cp.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+#cp -rs ./data ./../../../cppbackend-practice/sprint4/problems/leave_game/solution/data
+#cp -rs ./src ./../../../cppbackend-practice/sprint4/problems/leave_game/solution/src
+#cp -rs ./tests ./../../../cppbackend-practice/sprint4/problems/leave_game/solution/tests
+#cp -rs ./static ./../../../cppbackend-practice/sprint4/problems/leave_game/solution/static
+#
+#cp -rs ./CMakeLists.txt ./../../../cppbackend-practice/sprint4/problems/leave_game/solution/CMakeLists.txt
+#cp -rs ./conanfile.txt ./../../../cppbackend-practice/sprint4/problems/leave_game/solution/conanfile.txt
+#cp -rs ./Dockerfile ./../../../cppbackend-practice/sprint4/problems/leave_game/solution/Dockerfile
+
+# Define your destination once to save typing
+DEST="./../../../cppbackend-practice/sprint4/problems/leave_game/solution"
+
+# Create links using absolute source paths
+cp -fru "$(pwd)/data" "$DEST/data"
+cp -fru "$(pwd)/src" "$DEST/src"
+cp -fru "$(pwd)/tests" "$DEST/tests"
+cp -fru "$(pwd)/static" "$DEST/static"
+
+cp -fu "$(pwd)/CMakeLists.txt" "$DEST/CMakeLists.txt"
+cp -fu "$(pwd)/conanfile.txt" "$DEST/conanfile.txt"
+cp -fu "$(pwd)/Dockerfile" "$DEST/Dockerfile""
---
+++ sprint10_backend/leave_game/run_curl.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+
+printf ${GREEN}___join___${NC}
+echo ""
+TOKEN1=$(curl -s POST http://localhost:8080/api/v1/game/join \
+    -H "Content-Type: application/json" \
+    -d '{"userName":"Scooby1","mapId":"map1"}' \
+    | jq -r '.authToken')
+echo ""
+
+printf ${GREEN}___join___${NC}
+echo ""
+TOKEN2=$(curl -s POST http://localhost:8080/api/v1/game/join \
+    -H "Content-Type: application/json" \
+    -d '{"userName":"Scooby2","mapId":"town"}' \
+    | jq -r '.authToken')
+echo ""
+
+                                                                                                                                  
+for i in {1..3}
+do
+    echo "========================================"
+    echo "ITERATION: $i"
+    echo "========================================"
+
+#    printf "${GREEN}___action1___${NC}\n"
+#    curl -s POST http://localhost:8080/api/v1/game/player/action \
+#        -H "Content-Type: application/json" \
+#        -H "Authorization: Bearer $TOKEN1" \
+#        -d '{"move":"D"}' 
+#    echo ""
+#    
+#    wait
+
+    printf "${GREEN}___tick___${NC}\n"
+    curl -s POST http://localhost:8080/api/v1/game/tick  \
+        -H "Content-Type: application/json"  \
+        -d '{"timeDelta":100000}'
+    echo ""
+
+    printf "${GREEN}___state_pl1___${NC}\n"
+    curl -s GET http://localhost:8080/api/v1/game/state \
+        -H "Authorization: Bearer $TOKEN1"
+    echo -e "\n"
+
+    printf "${GREEN}___state_pl2___${NC}\n"
+    curl -s GET http://localhost:8080/api/v1/game/state \
+        -H "Authorization: Bearer $TOKEN2"
+    echo -e "\n"
+
+    printf "${GREEN}___maps/map1_1___${NC}\n"
+    curl -s GET http://localhost:8080/api/v1/maps/map1\
+        -H "Authorization: Bearer $TOKEN1"
+    echo -e "\n"
+
+    # sleep 0.1
+done
